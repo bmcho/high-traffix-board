aaef988175c938caf0aa8fff99e3ace68b34417d
bmcho <bmcho91@naver.com>
2025-08-05 17:19:22 +0900

    init: 기본 gradle, mysql docker-compose 설정

---
+++ src/main/java/com/bmcho/hightrafficboard/HighTrafficBoardApplication.java
@@ -0,0 +1,13 @@
+package com.bmcho.hightrafficboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighTrafficBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighTrafficBoardApplication.class, args);
+    }
+
+}
