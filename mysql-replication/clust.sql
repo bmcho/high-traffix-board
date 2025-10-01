6e6dcec63f300b08f2e699f04ed757a85c8ec2e8
bmcho <bmcho91@naver.com>
2025-10-01 17:35:21 +0900

    enh: mysql master / replica 간단 구현

---
+++ mysql-replication/clust.sql
@@ -0,0 +1,46 @@
+-- 1. Master 서버 SQL 5.7  이하
+GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%' IDENTIFIED BY '1q2w3e4r!!';
+FLUSH PRIVILEGES;
+SHOW MASTER STATUS;
+
+-- 8.0 이상
+CREATE USER 'replication_user'@'%' IDENTIFIED BY '1q2w3e4r!!';
+GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%';
+FLUSH PRIVILEGES;
+SHOW MASTER STATUS;
+
+-- 2. Slave 서버 SQL
+-- STOP SLAVE;
+-- RESET SLAVE ALL;
+
+-- 5.7
+CHANGE MASTER TO
+    MASTER_HOST='mysql-master',
+    MASTER_USER='replication_user',
+    MASTER_PASSWORD='1q2w3e4r!!',
+    MASTER_LOG_FILE='<TODO>',
+    MASTER_LOG_POS=<TODO>;
+START SLAVE;
+
+SHOW SLAVE STATUS;
+
+--8.0 이상
+STOP REPLICA;
+
+CHANGE REPLICATION SOURCE TO
+  SOURCE_HOST='mysql-master',
+  SOURCE_PORT=3306,
+  SOURCE_USER='replication_user',
+  SOURCE_PASSWORD='1q2w3e4r!!',
+  GET_SOURCE_PUBLIC_KEY = 1, -- 인증키 x
+  -- GTID 쓰면 아래 2줄 대신 AUTO_POSITION=1 사용
+  SOURCE_LOG_FILE='<TODO>',
+  SOURCE_LOG_POS=<TODO>;
+START REPLICA;
+
+SHOW REPLICA STATUS;
+
+
+SHOW TABLES;
+
+#
